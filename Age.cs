6ce3119356540d2083bc8f7b6e07865ca6c766b2
Chlorine-trifluoride <67011284+Chlorine-trifluoride@users.noreply.github.com>
2020-08-11 02:37:41 +0300

    Misc

---
+++ Age.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace HomeworkApp
+{
+    class Age
+    {
+        public int Years { get; set; }
+        public bool IsValid { get; set; }
+
+        // Lazy hack
+        public string SetValid()
+        {
+            IsValid = true;
+            return "Welcome to the secret club";
+        }
+    }
+}
---
+++ InputManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeworkApp
+{
+    class InputManager
+    {
+        public int GetYear(Renderer renderer)
+        {
+            int result;
+            bool success = false;
+
+            do
+            {
+                renderer.Render();
+                success = TryGetYear(out result);
+            } while (!success);
+
+            return result;
+        }
+
+        private bool TryGetYear(out int result)
+        {
+            string inputString = "";
+
+            void Clear()
+            {
+                inputString = "";
+                Renderer.Instance.Render();
+            }
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(false);
+
+                    // are done entering the year?
+                    if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        // is the length correct?
+                        if (inputString.Trim().Length == 4)
+                            break;
+                        else // clear and retry on incorrect input
+                            Clear();
+                    }
+
+                    else if (keyInfo.Key == ConsoleKey.Backspace)
+                    {   // easier to clear the whole thing on backspace
+                        Clear();
+                    }
+
+                    inputString += keyInfo.KeyChar;
+                }
+
+                else
+                {
+                    // clear the input if the window has had changes and re-render
+                    if (Renderer.Instance.CheckForResizeAndReRender())
+                        inputString = "";
+                }
+            }
+
+            return (int.TryParse(inputString, out result));
+        }
+
+        public int GetQuestionAnswer()
+        {
+            int selection = 0;
+            bool done = false;
+
+            do
+            {
+                // reset the keyinfo
+                ConsoleKeyInfo? keyInfo = null;
+
+                if (Console.KeyAvailable)
+                    keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo?.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        ModifySelection(ref selection, -1);
+                        Renderer.Instance.SetSelectionIdAndRender(selection);
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        ModifySelection(ref selection, 1);
+                        Renderer.Instance.SetSelectionIdAndRender(selection);
+                        break;
+
+                    case ConsoleKey.Enter:
+                        done = true;
+                        break;
+                }
+
+                Renderer.Instance.CheckForResizeAndReRender();
+                Thread.Sleep(16 * 2);
+            } while (!done);
+
+            return selection;
+        }
+
+        private void ModifySelection(ref int selection, int amount)
+        {
+            selection += amount;
+            int maxLen = QuestionLoader.Instance.CurrentQuestion.Options.Length - 1;
+
+            if (selection < 0)
+                selection = maxLen;
+
+            else if (selection > maxLen)
+                selection = 0;
+        }
+    }
+}
