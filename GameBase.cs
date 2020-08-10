4207dad3a5796f825321821b742d14f52765d0ce
Chlorine-trifluoride <67011284+Chlorine-trifluoride@users.noreply.github.com>
2020-08-11 02:36:47 +0300

    Hidden bloat

---
+++ GameBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkApp
+{
+    abstract class ApplicationBase
+    {
+        protected Renderer renderer;
+        protected InputManager inputMgr;
+
+        public ApplicationBase()
+        {
+            renderer = Renderer.Instance;
+            inputMgr = new InputManager();
+        }
+
+        public void Run()
+        {
+            Age age;
+            do
+            {
+                age = AgeVerification();
+                PrintAgeReply(age);
+            } while (!age.IsValid);
+
+            for (; ; )
+            {
+                PresentRandomQuestion();
+            }
+        }
+
+        protected abstract Age AgeVerification();
+        protected abstract void PrintAgeReply(Age age);
+        protected abstract void DisplayMessageToUser(string message, int milliseconds);
+        protected abstract void PresentRandomQuestion();
+    }
+}
